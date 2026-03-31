14a3e4f9a6332745ccf8f60fb5112053e98ae1d3
macbook-m4 <macbook-m4@MacBook-Air-de-macbook-m4.local>
2026-03-31 17:49:19 -0600

    añadiendo enseñanzas bios-vs-pneuma-identidad, dios-espacio-inmanencia, condescendencia-paciencia-desierto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'codice-v38'; // Subimos de versión para aplicar cambios
+const CACHE_NAME = 'codice-v40'; // Subimos de versión para aplicar cambios
 const ASSETS = [
   '/',
   '/index.html',
