5cbd9736afb0afc40f343093f6c6dd7f979c1e7b
macbook-m4 <macbook-m4@MacBook-Air-de-macbook-m4.local>
2026-04-13 18:24:06 -0600

    añadiendo lexicografia a etymos y enseñanza La Bio-Lógica de la Gracia: Inmerecido pero Diseñado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'codice-v45'; // Subimos de versión para aplicar cambios
+const CACHE_NAME = 'codice-v46'; // Subimos de versión para aplicar cambios
 const ASSETS = [
   '/',
   '/index.html',
