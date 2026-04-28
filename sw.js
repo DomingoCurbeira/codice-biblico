f18284bfdc1ceb204473d8d5f37035e1ace0d49b
macbook-m4 <macbook-m4@MacBook-Air-de-macbook-m4.local>
2026-04-28 10:59:28 -0600

    añadiendo gnosis y threskeia a Etymos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'codice-v59'; // Subimos de versión para aplicar cambios
+const CACHE_NAME = 'codice-v60'; // Subimos de versión para aplicar cambios
 const ASSETS = [
   '/',
   '/index.html',
